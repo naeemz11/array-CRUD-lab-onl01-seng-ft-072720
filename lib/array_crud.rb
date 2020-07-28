eb145edd6fc234efb1a4eb1aea42234e7e8901d2
Zannatul Naeem <mollahnaeem@gmail.com>
2020-07-28 23:34:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,11 +19,11 @@ end
 
 def remove_element_from_end_of_array(array)
   array= ["wow", "I", "am", "really", "learning","arrays!"]
-  element= array.pop
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array= ["wow", "I", "am", "really", "learning"]
 end
 
 def retrieve_element_from_index(array, index_number)
