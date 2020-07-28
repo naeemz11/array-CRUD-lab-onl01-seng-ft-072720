0d958e53c8797a2e7f17290abdad0cea99fa0120
Zannatul Naeem <mollahnaeem@gmail.com>
2020-07-28 23:38:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -29,6 +29,7 @@ end
 
 def retrieve_element_from_index(array, index_number)
   array= ["wow", "I", "am", "really", "learning", "arrays!"]
+  array.index[0]
 end
 
 def retrieve_first_element_from_array(array)
