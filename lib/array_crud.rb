2e93b24875db6f52cacb9c96ef4e56745bcee6ec
Zannatul Naeem <mollahnaeem@gmail.com>
2020-07-28 23:36:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -24,7 +24,7 @@ end
 
 def remove_element_from_start_of_array(array)
   array= ["wow", "I", "am", "really", "learning", "arrays!"]
-  arrays.shift 
+  array.shift 
 end
 
 def retrieve_element_from_index(array, index_number)
